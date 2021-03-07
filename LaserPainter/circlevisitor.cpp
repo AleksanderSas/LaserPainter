1add1fe520029d2c796900e82ddc33d8416cdf9e
AleksanderSas <aleksander.sas93@gmail.com>
2021-03-07 14:08:25 +0100

    minor changes

--- LaserPainter/circlevisitor.cpp
+++ LaserPainter/circlevisitor.cpp
@@ -38,12 +38,17 @@ const Point* CircleVisitor::next(std::vector<Point>& points)
     float r = hypotf(centre.x - p2.x, centre.y - p2.y);
     float offset = atan2f(p1.x - p2.x, p1.y - p2.y);
 
-    float angel = offset + tInComponent * 1.5 * 2 * 3.1415;
+    float angel = offset + tInComponent * 1.5f * 2 * 3.1415f;
+
     p.x = centre.x + r * sinf(angel);
     p.y = centre.y + r * cosf(angel);
     p.enableLaser = p1.enableLaser;
 
     tInComponent += deltaT;
+    if(tInComponent > 0.66f)
+    {
+        tInComponent += deltaT / 2;
+    }
 
     if(tInComponent > 1.0f)
     {
