452f0f66280b07263f494af092ea9a32f5c5db99
AleksanderSas <aleksander.sas93@gmail.com>
2022-01-29 15:36:18 +0100

    Allow scaling

--- LaserPainter/Ui/multipleselectionmanager.cpp
+++ LaserPainter/Ui/multipleselectionmanager.cpp
@@ -8,8 +8,19 @@ MultipleSelectionManager::MultipleSelectionManager()
 
 bool MultipleSelectionManager::onMousePress(ShapeCollection &shapeCollection, int x, int y, Qt::KeyboardModifiers modifier)
 {
-    selectedManyStartX = selectedManyEndX = x;
-    selectedManyStartY = selectedManyEndY = y;
+    if(modifier == Qt::NoModifier && isRectangleCorner(x, y))
+    {
+        selectManyMode = SelectedManyMode::scaling;
+        originalScaling.clear();
+        for(Point* p: selectedPointCondidates)
+        {
+            originalScaling.push_back(*p);
+        }
+        return true;
+    }
+
+    rectangleStartX = rectangleEndX = selectedManyStartX = selectedManyEndX = x;
+    rectangleStartY = rectangleEndY = selectedManyStartY = selectedManyEndY = y;
     auto cord = toCollectionPoint2(x, y);
     if(modifier == Qt::NoModifier)
     {
@@ -61,16 +72,80 @@ bool MultipleSelectionManager::onMouseRelease(UnReDoPanel *unredoPanle, Operatio
     if(selectManyMode == SelectedManyMode::selecting)
     {
         selectedPoints.insert(selectedPointCondidates.begin(), selectedPointCondidates.end());
-        selectedPointCondidates.clear();
-        selectManyMode = SelectedManyMode::moving;
+        selectManyMode = SelectedManyMode::moving_or_scaling;
         return true;
     }
+    selectedManyStartX = rectangleStartX;
+    selectedManyStartY = rectangleStartY;
+    selectedManyEndX = rectangleEndX;
+    selectedManyEndY = rectangleEndY;
 
     return false;
 }
 
+void MultipleSelectionManager::scalePoints(float scaleX, float scaleY, int anchoreX, int anchoreY)
+{
+    auto anchor = toCollectionPoint2(anchoreX, anchoreY);
+    anchoreX = anchor.first;
+    anchoreY = anchor.second;
+    auto original = originalScaling.begin();
+    for(auto &p : selectedPointCondidates)
+    {
+        Point& originalVal = *original;
+        p->x = anchoreX - (anchoreX - originalVal.x) * scaleX;
+        p->y = anchoreY - (anchoreY - originalVal.y) * scaleY;
+        original++;
+    }
+}
+
+void MultipleSelectionManager::scale(int x, int y)
+{
+    float xScale = 1.0f;
+    float yScale = 1.0f;
+
+    switch (scalingPoint) {
+    case ScalingPoint::start_start:
+        rectangleStartX = x;
+        rectangleStartY = y;
+        xScale = 1.0f * (selectedManyEndX - x ) / (selectedManyEndX - selectedManyStartX);
+        yScale = 1.0f * (selectedManyEndY - y ) / (selectedManyEndY - selectedManyStartY);
+        scalePoints(xScale, yScale, selectedManyEndX, selectedManyEndY);
+        break;
+
+    case ScalingPoint::start_end:
+        rectangleStartX = x;
+        rectangleEndY = y;
+        xScale = 1.0f * (selectedManyEndX - x ) / (selectedManyEndX - selectedManyStartX);
+        yScale = 1.0f * (y - selectedManyStartY ) / (selectedManyEndY - selectedManyStartY);
+        scalePoints(xScale, yScale, selectedManyEndX, selectedManyStartY);
+        break;
+
+    case ScalingPoint::end_start:
+        rectangleEndX = x;
+        rectangleStartY = y;
+        xScale = 1.0f * (x - selectedManyStartX) / (selectedManyEndX - selectedManyStartX);
+        yScale = 1.0f * (selectedManyEndY - y ) / (selectedManyEndY - selectedManyStartY);
+        scalePoints(xScale, yScale, selectedManyStartX, selectedManyEndY);
+        break;
+
+    case ScalingPoint::end_end:
+        rectangleEndX = x;
+        rectangleEndY = y;
+        xScale = 1.0f * (x - selectedManyStartX) / (selectedManyEndX - selectedManyStartX);
+        yScale = 1.0f * (y - selectedManyStartY ) / (selectedManyEndY - selectedManyStartY);
+        scalePoints(xScale, yScale, selectedManyStartX, selectedManyStartY);
+        break;
+    }
+}
+
 bool MultipleSelectionManager::onMouseMove(int x, int y, ShapeCollection &shapeCollection)
 {
+    if(selectManyMode == SelectedManyMode::scaling)
+    {
+        scale(x, y);
+        return true;
+    }
+
     if(selectManyMode == SelectedManyMode::selecting)
     {
         selectedPointCondidates.clear();
@@ -82,8 +157,8 @@ bool MultipleSelectionManager::onMouseMove(int x, int y, ShapeCollection &shapeC
                 selectedPointCondidates.insert(&p);
             }
         }
-        selectedManyEndX = x;
-        selectedManyEndY = y;
+        rectangleEndX = selectedManyEndX = x;
+        rectangleEndY = selectedManyEndY = y;
 
         return true;
     }
@@ -96,8 +171,8 @@ bool MultipleSelectionManager::onMouseMove(int x, int y, ShapeCollection &shapeC
             point->x += cord.first;
             point->y += cord.second;
         }
-        selectedManyEndX = x;
-        selectedManyEndY = y;
+        rectangleEndX = selectedManyEndX = x;
+        rectangleEndY = selectedManyEndY = y;
         return true;
     }
     return false;
@@ -107,13 +182,15 @@ void MultipleSelectionManager::clear()
 {
     selectedPointCondidates.clear();
     selectedPoints.clear();
+    rectangleStartX = rectangleEndX = selectedManyStartX = selectedManyEndX =  -100;
+    rectangleStartY = rectangleEndY = selectedManyStartY = selectedManyEndY = -100;
 }
 
 void MultipleSelectionManager::drawSelectionRectangle(QPainter &painter)
 {
-    if(selectManyMode == SelectedManyMode::selecting)
+    if(selectManyMode == SelectedManyMode::selecting || selectManyMode == SelectedManyMode::scaling || selectManyMode == SelectedManyMode::moving_or_scaling)
     {
-        painter.drawRect(selectedManyStartX, selectedManyStartY, selectedManyEndX - selectedManyStartX, selectedManyEndY - selectedManyStartY);
+        painter.drawRect(rectangleStartX, rectangleStartY, rectangleEndX - rectangleStartX, rectangleEndY- rectangleStartY);
     }
 }
 
@@ -129,12 +206,42 @@ bool MultipleSelectionManager::isInsideSelectedArea(int x, int y)
     return myXor(x > selectedManyStartX, x > selectedManyEndX) && myXor(y > selectedManyStartY, y > selectedManyEndY);
 }
 
+bool inline isClose(int x1, int y1, int x2, int y2)
+{
+    int dist = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    return dist < 2500;
+}
+
+bool MultipleSelectionManager::isRectangleCorner(int x, int y)
+{
+    if(isClose(selectedManyStartX, selectedManyStartY, x, y))
+    {
+        scalingPoint = ScalingPoint::start_start;
+        return true;
+    }
+    if(isClose(selectedManyStartX, selectedManyEndY, x, y))
+    {
+        scalingPoint = ScalingPoint::start_end;
+        return true;
+    }
+    if(isClose(selectedManyEndX, selectedManyStartY, x, y))
+    {
+        scalingPoint = ScalingPoint::end_start;
+        return true;
+    }
+    if(isClose(selectedManyEndX, selectedManyEndY, x, y))
+    {
+        scalingPoint = ScalingPoint::end_end;
+        return true;
+    }
+    return false;
+}
+
 bool MultipleSelectionManager::isCloseToAnySelected(int x, int y)
 {
     for(auto &p: selectedPoints)
     {
-        unsigned int dist = (p->x - x) * (p->x - x) + (p->y - y) * (p->y - y);
-        if(dist < 2500)
+        if(isClose(p->x, p->y, x, y))
         {
             return true;
         }
--- LaserPainter/Ui/multipleselectionmanager.h
+++ LaserPainter/Ui/multipleselectionmanager.h
@@ -2,11 +2,13 @@
 #define MULTIPLESELECTIONMANAGER_H
 
 #include <set>
+#include <vector>
 #include <QPainter>
 #include "../Collection/shapecollection.h"
 #include "unredopanel.h"
 
-enum SelectedManyMode{ no, selecting, selecting_ctrl, moving};
+enum SelectedManyMode{ no, selecting, selecting_ctrl, moving_or_scaling, moving, scaling};
+enum ScalingPoint{ start_start, start_end, end_start, end_end};
 
 class MultipleSelectionManager
 {
@@ -22,10 +24,16 @@ public:
 
 private:
     int selectedManyStartX, selectedManyStartY, selectedManyEndX, selectedManyEndY;
+    int rectangleStartX, rectangleStartY, rectangleEndX, rectangleEndY;
     SelectedManyMode selectManyMode = SelectedManyMode::no;
+    ScalingPoint scalingPoint;
     std::set<Point*> selectedPoints;
     std::set<Point*> selectedPointCondidates;
+    std::vector<Point> originalScaling;
+    void scale(int x, int y);
+    void scalePoints(float scaleX, float scaleY, int anchoreX, int anchoreY);
 
+    bool isRectangleCorner(int x, int y);
     bool isCloseToAnySelected(int x, int y);
     bool isInsideSelectedArea(int x, int y);
 };
